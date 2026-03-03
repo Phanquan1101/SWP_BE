379f1049e253ff82348649d3505137be9f600228
Phan Doan The Quan <quanpdtde180164@fpt.edu.vn>
2026-03-03 11:09:52 +0700

    Update GlobalException

--- src/main/java/com/crowdsourced/wasteplatform/WastePlatformApplication.java
+++ src/main/java/com/crowdsourced/wasteplatform/WastePlatformApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WastePlatformApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WastePlatformApplication.class, args);
     }
 
